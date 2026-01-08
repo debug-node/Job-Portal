a9b7eb6d7186b5b2f4f7508a05fdedb423c08a6a
debug-node <anupraj.2085@gmail.com>
2026-01-08 21:27:18 +0530

    feat: add JWT token generation to user registration and add login controller

--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -4,6 +4,7 @@ import { sql } from "../utils/db.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { TryCatch } from "../utils/TryCatch.js";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const registerUser = TryCatch(async (req, res, next) => {
     const { name, email, password, phoneNumber, role, bio } = req.body;
@@ -48,8 +49,59 @@ export const registerUser = TryCatch(async (req, res, next) => {
         registeredUser = user;
     }
 
+    const token = jwt.sign(
+        { id: registeredUser?.user_id },
+        process.env.JWT_SEC as string,
+        {
+            expiresIn: "15d",
+        }
+    );
+
     res.json({
         message: "User registered successfully",
         registeredUser,
+        token,
     });
 })
+
+export const loginUser = TryCatch(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        throw new ErrorHandler(400, "Email and password are required");
+    }
+
+    const user = await sql`
+    SELECT u.user_id, u.name, u.email, u.password, u.phone_number, u.role, u.bio, u.resume, u.profile_pic, u.subscription, ARRAY_AGG(s.name) FILTER (WHERE s.name IS NOT NULL) as skills FROM users u LEFT JOIN user_skills us ON u.user_id = us.user_id LEFT JOIN skills s ON us.skill_id = s.skill_id WHERE u.email = ${email} GROUP BY u.user_id;
+    `;
+
+    if (user.length === 0) {
+        throw new ErrorHandler(400, "Invalid credentials");
+    }
+
+    const userObject = user[0];
+
+    const matchPassword = await bcrypt.compare(password, userObject.password);
+
+    if (!matchPassword) {
+        throw new ErrorHandler(400, "Invalid credentials");
+    }
+
+    userObject.skills = userObject.skills || [];
+
+    delete userObject.password;
+
+    const token = jwt.sign(
+        { id: userObject?.user_id },
+        process.env.JWT_SEC as string,
+        {
+            expiresIn: "15d",
+        }
+    );
+
+    res.json({
+        message: "User logged in successfully",
+        userObject,
+        token,
+    });
+});
